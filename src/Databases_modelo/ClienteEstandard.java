f9258f34b9305054dee0a1200ff3317c509c33e7
jfontvit <jordifv90@gmail.com>
2023-03-29 12:51:17 +0200

    Add ClienteEstandard and ClientePremium subclasses

--- src/Databases_modelo/ClienteEstandard.java
+++ src/Databases_modelo/ClienteEstandard.java
@@ -1,4 +1,20 @@
 package Databases_modelo;
 
-public class ClienteEstandard {
+import enums.ClienteTipo;
+
+public class ClienteEstandard extends Cliente {
+
+    public ClienteEstandard(String nombre, String apellidos, String domicilio, String nif, String email, ClienteTipo tipo) {
+        super(nombre, apellidos, domicilio, nif, email, ClienteTipo.ESTANDARD);
+    }
+
+    @Override
+    public float calcAnual() {
+        return 0;
+    }
+
+    @Override
+    public float descuentoEnv() {
+        return 0;
+    }
 }
--- src/Databases_modelo/ClientePremium.java
+++ src/Databases_modelo/ClientePremium.java
@@ -1,4 +1,18 @@
 package Databases_modelo;
 
-public class ClientePremium {
+import enums.ClienteTipo;
+
+public class ClientePremium extends Cliente {
+
+    public ClientePremium(String nombre, String apellidos, String domicilio, String nif, String email, ClienteTipo tipo) {
+        super(nombre, apellidos, domicilio, nif, email, ClienteTipo.PREMIUM);
+    }
+
+    public float calcAnual() {
+        return this.tipo == ClienteTipo.PREMIUM ? 20 : 0;
+    }
+
+    public float descuentoEnv(){
+        return this.tipo == ClienteTipo.PREMIUM ? 30 : 0;
+    }
 }
