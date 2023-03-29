e04144aa99f833abfe9662e064206e9cc8f25577
jfontvit <jordifv90@gmail.com>
2023-03-29 12:59:00 +0200

    Add GestionOs in OnlineStore main method

--- src/Databases_controlador/OnlineStore.java
+++ src/Databases_controlador/OnlineStore.java
@@ -1,7 +1,10 @@
 package Databases_controlador;
 
+import Databases_vista.GestionOS;
+
 public class OnlineStore {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GestionOS gestion = new GestionOS();
+        gestion.inicio();
     }
 }
