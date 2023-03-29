8b4ca6284323af8ea935874dd38e72443362f584
jfontvit <jordifv90@gmail.com>
2023-03-29 12:56:43 +0200

    Add Datos model, with data entry method and add/get/exist/length Client methods

--- src/Databases_modelo/Datos.java
+++ src/Databases_modelo/Datos.java
@@ -1,4 +1,79 @@
 package Databases_modelo;
 
+import enums.ClienteTipo;
+
+import java.util.ArrayList;
+
 public class Datos {
+    private ListaArticulos listaArticulos;
+    private ListaClientes listaClientes;
+    private ListaPedidos listaPedidos;
+
+    public Datos(){
+        listaArticulos = new ListaArticulos();
+        listaClientes = new ListaClientes();
+        listaPedidos = new ListaPedidos();
+
+        cargarDatos();
+    }
+
+    public void cargarDatos(){
+        Cliente clientePremium1 = new ClientePremium("Jordi","Font","C/Rocafort 125","12345678A","jordi@gmail.com",ClienteTipo.PREMIUM);
+        try {
+            listaClientes.add(clientePremium1);
+            ClientePremium clientePremium2 = new ClientePremium("Abel","Gimenez","C/Calabria 134","98763121B","abel@gmail.com",ClienteTipo.PREMIUM);
+            listaClientes.add(clientePremium2);
+            ClienteEstandard clienteEstandard1 = new ClienteEstandard("David","Gonzalez","C/Cantabria 92","12356732C","david@gmail.com",ClienteTipo.ESTANDARD);
+            listaClientes.add(clienteEstandard1);
+            ClienteEstandard clienteEstandard2 = new ClienteEstandard("Marta","Rodriguez","C/Mallorca 22","6355232D","marta@gmail.com",ClienteTipo.ESTANDARD);
+            listaClientes.add(clienteEstandard2);
+        }catch (Exception e){
+            System.out.println("La carga de datos ha fallado");
+        }
+
+    }
+
+    public ArrayList<Cliente> getCustomers() {
+        return this.listaClientes.getArrayList();
+    }
+
+    public ArrayList<Cliente> getPremiumCustomers(){
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+        for (int i=0; i < this.listaClientes.getSize(); i++){
+            if (this.listaClientes.getAt(i).getTipo() == ClienteTipo.PREMIUM){
+                clientes.add(this.listaClientes.getAt(i));
+            }
+        }
+        return clientes;
+    }
+
+    public ArrayList<Cliente> getStandardCustomers(){
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+        for (int i=0; i < this.listaClientes.getSize(); i++){
+            if (this.listaClientes.getAt(i).getTipo() == ClienteTipo.ESTANDARD){
+                clientes.add(this.listaClientes.getAt(i));
+            }
+        }
+        return clientes;
+    }
+
+    public void customerAdd(Cliente cliente) throws Exception {
+        try {
+            this.listaClientes.add(cliente);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean customerExists(String email){
+        return this.listaClientes.contains(email);
+    }
+
+    public int customerLength(){
+        return this.listaClientes.getSize();
+    }
+
+
 }
