bb3cdda219153ab060578806120605737ab7058e
jfontvit <jordifv90@gmail.com>
2023-05-02 13:10:53 +0200

    Add the other two hibernateDao, implementation required

---
+++ src/Databases_dao/Hibernate_DAO/HibernateArticuloDao.java
@@ -0,0 +1,7 @@
+package Databases_dao.Hibernate_DAO;
+
+import Databases_dao.ArticuloDAO;
+
+// hay que implementar los metodos de ArticuloDAO y quitar que la clase sea abstract
+public abstract class HibernateArticuloDao implements ArticuloDAO {
+}
---
+++ src/Databases_dao/Hibernate_DAO/HibernatePedidoDao.java
@@ -0,0 +1,7 @@
+package Databases_dao.Hibernate_DAO;
+
+import Databases_dao.PedidoDAO;
+
+// hay que implementar los metodos de PedidoDAO y quitar que la clase sea abstract
+public abstract class HibernatePedidoDao implements PedidoDAO {
+}
